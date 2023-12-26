4e275199820f8476b365e7273ee15d960d0cceca
Joel Salas <joel.salas.093@gmail.com>
2023-12-26 14:39:28 -0500

    Portfolio Entity completed

--- src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -1,1 +1,51 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.sql.Date;
+
+@Entity
+public class Portfolio {
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @Column(nullable = false)
+    private long clientId;
+
+    @Column(nullable = false)
+    private Date creationDate;
+
+
+    protected Portfolio() {
+
+    }
+
+    public Portfolio(long clientId, long portfolioId, Date creationDate) {
+        this.clientId = clientId;
+        this.portfolioId = portfolioId;
+        this.creationDate = creationDate;
+
+    }
+
+    public long getPortfolioId() {
+        return portfolioId;
+    }
+
+
+    public long getClientId() {
+        return clientId;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+}
 
